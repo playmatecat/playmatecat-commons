857fa74c1ddeb74eb8112c19d36b940a8e94410c
Isa <Isa@Isa-PC>
2015-05-24 01:13:49 +0800

    1

--- src/main/java/com/playmatecat/cas/ctrl/SubSysLoginController.java
+++ src/main/java/com/playmatecat/cas/ctrl/SubSysLoginController.java
@@ -1,9 +1,12 @@
 package com.playmatecat.cas.ctrl;
 
+import java.util.Properties;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.web.util.WebUtils;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,10 +22,29 @@ public class SubSysLoginController {
 	@RequestMapping("/cas-login")
 	public String casLogin(HttpServletRequest request, HttpServletResponse response) {
 		//获得跳转登录前访问的最后地址
-		String lastUrl = WebUtils.getSavedRequest(request).getRequestUrl();
+		String lastUrl;
+		if(WebUtils.getSavedRequest(request) == null) {
+			lastUrl = "";
+		} else {
+			lastUrl = WebUtils.getSavedRequest(request).getRequestUrl();
+			lastUrl = lastUrl == null ? "" : lastUrl;
+		}
+
+		Properties props = null;
+		try {
+			props = PropertiesLoaderUtils.loadAllProperties("/config/cas/cas.properties");
+        } catch (Exception e) {
+	        e.printStackTrace();
+        }
+		
+		if(props.isEmpty()) {
+			return null;
+		}
+		
+		String casServerUrl = props.getProperty("cas.server.url");
 		
 		//跳转到cas请求登录验证
-		String casUrl = "cas server loginUrl" + "?url=" + lastUrl;
+		String casUrl = "redirect:" + casServerUrl + "?url=" + lastUrl;
 		return casUrl;
 	}
 }
