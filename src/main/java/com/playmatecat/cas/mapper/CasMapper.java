bf181670ffce796184256d91463327ffc69b9cca
Isa <Isa@Isa-PC>
2015-05-24 17:18:08 +0800

    1

---
+++ src/main/java/com/playmatecat/cas/mapper/CasMapper.java
@@ -0,0 +1,10 @@
+package com.playmatecat.cas.mapper;
+
+/**
+ * cas mybatis接口
+ * @author blackcat
+ *
+ */
+public interface CasMapper {
+	public int doSomething(int id);
+}
---
+++ src/main/java/com/playmatecat/utils/spring/UtilsSpringContext.java
@@ -0,0 +1,44 @@
+package com.playmatecat.utils.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * <pre>
+ * spring上下文工具,用于获得spring上下文
+ * 需要在xml中配置一下这个类才能启用
+ * bean class="com.playmatecat.utils.spring.UtilsSpringContext" id = "UtilsSpringContext"
+ * spring会自动注入这个类
+ * </pre>
+ * @author blackcat
+ *
+ */
+public class UtilsSpringContext implements ApplicationContextAware {
+
+	@Autowired
+	private static ApplicationContext applicationContext;
+	
+	public static Object getBean(String beanName){
+		applicationContext.getBeanDefinitionNames();
+		Object obj = null;
+		try {
+			obj = applicationContext.getBean(beanName);
+        } catch (Exception e) {
+	        //do nothing
+        }
+
+		return obj;		
+	}
+
+	public static <T> T getBean(Class<T> clazz){
+		return (T)applicationContext.getBean(clazz);		
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		UtilsSpringContext.applicationContext=applicationContext;
+	}
+	
+}
