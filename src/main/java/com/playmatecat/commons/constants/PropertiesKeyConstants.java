18dd735ba777ec660d97c258b3481fe8394a47ad
blackcatIan <blackcatIan@hotmail.com>
2015-06-11 13:17:35 +0800

    1

--- src/main/java/com/playmatecat/commons/constants/PropertiesKeyConstants.java
+++ src/main/java/com/playmatecat/commons/constants/PropertiesKeyConstants.java
@@ -1,5 +1,7 @@
 package com.playmatecat.commons.constants;
 
+import com.playmatecat.mina.servletListener.MinaClientContextListener;
+
 /**
  * 通用的参数key
  * @author blackcat
@@ -15,4 +17,8 @@ public interface PropertiesKeyConstants {
     
     /**子系统的权限数据库名**/
     public final static String CAS_SUBSYS_SYS_DATABASE = "cas.subsys.sys.database";
+    
+    /**mina服务器列表(包括端口),Key名:IP:端口,格式例如: customName:127.0.0.1:8501,逗号分割
+     * @see MinaClientContextListener#contextInitialized(javax.servlet.ServletContextEvent)**/
+    public final static String MINA_SERVICE_ADDRESS_LIST = "mina.service.address.list";
 }
--- src/main/java/com/playmatecat/mina/servletListener/MinaClientContextListener.java
+++ src/main/java/com/playmatecat/mina/servletListener/MinaClientContextListener.java
@@ -1,6 +1,8 @@
 package com.playmatecat.mina.servletListener;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -9,14 +11,31 @@ import com.playmatecat.commons.constants.PropertiesKeyConstants;
 import com.playmatecat.mina.client.NioTcpClient2;
 import com.playmatecat.utils.spring.UtilsProperties;
 
+/**
+ * 初始化client端的mina服务
+ * @author blackcat
+ *
+ */
 public class MinaClientContextListener implements ServletContextListener{
     
     private HashMap<String, NioTcpClient2> nioClientMap = new HashMap<String, NioTcpClient2>();
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("my listener !!!!!!!!!!!!!!!!!!!!");
-        System.out.println(UtilsProperties.getProp(PropertiesKeyConstants.CAS_SUBSYS_SYS_DATABASE));
+        //获得mina address配置参数
+        String minaServerAddr = UtilsProperties.getProp(PropertiesKeyConstants.MINA_SERVICE_ADDRESS_LIST);
+        String[] serverAddrArray = minaServerAddr.split(",");
+        List<String> serverList = Arrays.asList(serverAddrArray);
+        
+        //分解读取参数
+        serverList.stream().forEach(peekServer -> {
+            String[] ServerArgs = peekServer.split(":");
+            String KeyName = ServerArgs[0];
+            String Address = ServerArgs[1];
+            Integer port = Integer.valueOf(ServerArgs[2]);
+            //TODO 完成服务器分配
+            
+        });
     }
 
     @Override
